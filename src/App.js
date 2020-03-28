6742760e8b98e887191fb7f4152f251e2c617f82
Anand Raj <anandamritraj904@gmail.com>
2020-03-28 23:29:44 +0530

    PWA test. (Fingers crossed)

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, Suspense } from "react";
 import "./App.css";
 import { getStateWiseData, getStats } from "./services/patients.service";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
@@ -6,11 +6,12 @@ import Help from "./pages/Help";
 import Home from "./pages/Home";
 import Toolbar from "./components/nav/Toolbar";
 import Footer from "./components/Footer";
-import Credits from "./pages/Credits";
-import Notification from "./components/Notification";
-import FAQs from "./pages/FAQs";
 import GoogleAnalytics from "./components/GoogleAnalytics";
 import WhatsappShare from "./components/whatsapp-share";
+
+const Notification = React.lazy(() => import("./components/Notification"));
+const Credits = React.lazy(() => import("./pages/Credits"));
+const FAQs = React.lazy(() => import("./pages/FAQs"));
 class App extends Component {
   constructor() {
     super();
@@ -54,38 +55,40 @@ class App extends Component {
   render() {
     return (
       <Router>
-        <div>
-          <Toolbar />
-          <Notification></Notification>
-          <Switch>
-            <Route exact path="/help">
-              <Help></Help>
-            </Route>
-            <Route exact path="/credits">
-              <Credits></Credits>
-            </Route>
-            <Route exact path="/faqs">
-              <FAQs></FAQs>
-            </Route>
-            <Route exact path="/">
-              <Home
-                isLoading={this.state.isLoading}
-                dayChange={this.state.dayChange}
-                total={this.state.total}
-                tested={this.state.tested}
-                statewise={this.state.statewise}
-                isMobile={this.state.isMobile}
-                ageGroup={this.state.ageGroup}
-                nationality={this.state.nationality}
-                gender={this.state.gender}
-                hospitalizationStatus={this.state.hospitalizationStatus}
-              ></Home>
-            </Route>
-          </Switch>
-          <Footer></Footer>
-        </div>
-        <GoogleAnalytics></GoogleAnalytics>
-        <WhatsappShare></WhatsappShare>
+        <Suspense fallback={<div>Loading...</div>}>
+          <div>
+            <Toolbar />
+            <Notification></Notification>
+            <Switch>
+              <Route exact path="/help">
+                <Help></Help>
+              </Route>
+              <Route exact path="/credits">
+                <Credits></Credits>
+              </Route>
+              <Route exact path="/faqs">
+                <FAQs></FAQs>
+              </Route>
+              <Route exact path="/">
+                <Home
+                  isLoading={this.state.isLoading}
+                  dayChange={this.state.dayChange}
+                  total={this.state.total}
+                  tested={this.state.tested}
+                  statewise={this.state.statewise}
+                  isMobile={this.state.isMobile}
+                  ageGroup={this.state.ageGroup}
+                  nationality={this.state.nationality}
+                  gender={this.state.gender}
+                  hospitalizationStatus={this.state.hospitalizationStatus}
+                ></Home>
+              </Route>
+            </Switch>
+            <Footer></Footer>
+          </div>
+          <GoogleAnalytics></GoogleAnalytics>
+          <WhatsappShare></WhatsappShare>
+        </Suspense>
       </Router>
     );
   }
