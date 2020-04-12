631765742e5103544bd111264c1e314a6d236e65
varull <varull97@gmail.com>
2020-04-12 19:15:43 +0530

    Add files via upload

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -1,7 +1,13 @@
 import React from "react";
 
 const Stats = props => {
+  // var totalconfirmedpercent = {props.total.confirmed}/{props.tested.totalsamplestested};
+  const totalconfirmedpercent = (props.total.confirmed/props.tested.totalsamplestested).toPrecision(2);;
+  // const totalconfirmedpercent = props.total.confirmed/props.tested.totalsamplestested;
+  // const totalconfirmedpercent = props.total.confirmed/props.tested.totalsamplestested;
+  // const totalconfirmedpercent = props.total.confirmed/props.tested.totalsamplestested;
   return (
+
     <div className="row stats">
       <div className="col-3 col-6-sm card confirmed">
         <p className="delta">(+{props.dayChange.confirmed})</p>
@@ -23,7 +29,27 @@ const Stats = props => {
         <p className="count">{props.total.deaths}</p>
         <p className="title">Deceased</p>
       </div>
+
+      <div className="col-4 col-6-sm card confirmed">
+        <p className="delta">(+{(100*props.dayChange.confirmed/props.tested.delta).toPrecision(3)}) %</p>
+        <p className="count">{(100*props.total.confirmed/props.tested.totalsamplestested).toPrecision(3)} %</p>
+        <p className="title">Confirmed Cases</p>
+      </div>
+
+      <div className="col-4 col-6-sm card recovered">
+        <p className="delta">(+{(100*props.dayChange.recovered/props.dayChange.confirmed).toPrecision(3)})</p>
+        <p className="count">{(100*props.total.recovered/props.total.confirmed).toPrecision(3)} %</p>
+        <p className="title">Recovered </p>
+      </div>
+      <div className="col-4 col-6-sm card deceased">
+        <p className="delta">(+{(100*props.dayChange.deceased/props.dayChange.confirmed).toPrecision(3)})</p>
+        <p className="count">{(100*props.total.deaths/props.total.confirmed).toPrecision(3)} %</p>
+        <p className="title">Deceased </p>
+      </div>
     </div>
+
+
+
   );
 };
 
