25e74f4abc333f505e243f00eaf760deb58d5b9e
Akshay Alenchery <akshayalenchery@gmail.com>
2020-04-23 20:06:57 +0530

    Component to display testing data

---
+++ src/components/StateWise/StateWiseTestData.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { useTranslation } from "react-i18next";
+import { format, parse } from "date-fns";
+
+export default ({testingData}) => {
+  const { t } = useTranslation()
+  return (
+    <div>
+      <div className="state-desc-header">
+        <div className="last-updated-state">
+          {t('Updated on')}{" "}
+          {format(parse(testingData.updatedon, 'dd/MM/yyyy', new Date()),'dd MMM yyyy')}
+        </div>
+      </div>
+      <table>
+        <thead>
+          <tr>
+            <th>Total Tests</th>
+            <th>Positive</th>
+            <th>Negative</th>
+            <th>Unconfirmed</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>{testingData.totaltested}</td>
+            <td>{testingData.positive}</td>
+            <td>{testingData.negative}</td>
+            <td>{testingData.unconfirmed}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  )
+}
