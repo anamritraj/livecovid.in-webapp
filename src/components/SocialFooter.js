9868750e157a5755e6217523d9f4977136ed10de
Anand Raj <anandamritraj904@gmail.com>
2020-04-26 14:24:04 +0530

    Remove unused shares, (Based on GA data). Add slack channel

--- src/components/SocialFooter.js
+++ src/components/SocialFooter.js
@@ -2,21 +2,37 @@ import React from 'react';
 
 import Github from '../components/nav/github.svg'
 import Twitter from '../components/nav/twitter.svg'
+import Slack from '../components/nav/slack.svg'
+import Whatsapp from '../components/nav/whatsapp.svg'
+
 import { useTranslation } from 'react-i18next';
+import { sendEventToGA } from '../services/analytics.service';
+
+const handleOnclick  = (app) => {
+  sendEventToGA('User', 'Clicked Share/'+ app, app)
+}
 
 const SocialFooter = () => {
   const {t} = useTranslation();
   return <div className="social-footer">
     <ul>
-      <li><a className="github" target="_blank" rel="noopener noreferrer" href="https://github.com/anamritraj/livecovid.in-webapp">
+      <li><a className="github" target="_blank" rel="noopener noreferrer" onClick={() => handleOnclick('github')} href="https://github.com/anamritraj/livecovid.in-webapp">
         <img src={Github} alt="github share icon" />
         {t('Contribute on Github')}
         </a></li>
-
-      <li><a className="twitter" target="_blank" rel="noopener noreferrer" href="https://twitter.com/share?text=Covid-19 India provides the latest state-wise updates on the spread of the coronavirus in India. Stay Informed. Stay Inside!&url=https://livecovid.in">
+      <li><a className="slack" target="_blank" rel="noopener noreferrer" onClick={() => handleOnclick('slack')} href="https://join.slack.com/t/livecovidin/shared_invite/zt-e10jouqp-HquZhM2L853zaN0JM9S2qg">
+        <img src={Slack} alt="slack join" />
+        {t('Join on Slack')}
+      </a></li>
+      <li><a className="twitter" target="_blank" rel="noopener noreferrer" onClick={() => handleOnclick('twitter')} href="https://twitter.com/share?text=Covid-19 India provides the latest state-wise updates on the spread of the coronavirus in India. Stay Informed. Stay Inside!&url=https://livecovid.in">
         <img src={Twitter} alt="twitter share icon" />
         {t('Share on Twitter')}
         </a></li>
+      <li><a className="whatsapp" data-action="share/whatsapp/share" target="_blank" rel="noopener noreferrer" onClick={() => handleOnclick('whatsapp')} href="whatsapp://send?text=Covid-19 India provides the latest state-wise updates on the spread of the coronavirus in India. Stay Informed. Stay Inside https://livecovid.in/">
+        <img src={Whatsapp} alt="whatsapp join" />
+        {t('Share on Whatsapp')}
+      </a></li>
+
     </ul>
   </div>
 }
