6f785075a0cb49d1f96c70ba6ab99fa765a2c869
Anand Amrit Raj <anamritraj@users.noreply.github.com>
2020-05-03 06:23:00 +0530

    Add more data to district table, fix styling with testing data (#19)
    * Add more data to district table, fix styling with testing data
    
    * Fix dark mode issues, fix district table reponsiveness

--- src/components/StateWise/StateWiseTestData.js
+++ src/components/StateWise/StateWiseTestData.js
@@ -1,35 +1,15 @@
 import React from 'react'
 import { useTranslation } from "react-i18next";
-import { format, parse } from "date-fns";
 
 export default ({testingData}) => {
   const { t } = useTranslation()
+
   return (
-    <div>
-      <div className="state-desc-header">
-        <div className="last-updated-state">
-          {t('Updated on')}{" "}
-          {format(parse(testingData.updatedon, 'dd/MM/yyyy', new Date()),'dd MMM yyyy')}
-        </div>
+    <>
+      <div className="testing-stats">
+        <span>{t('Tested')}</span><br />
+        {testingData && testingData.totaltested ? Number(testingData.totaltested).toLocaleString('en'): "-"}
       </div>
-      <table>
-        <thead>
-          <tr>
-            <th>Total Tests</th>
-            <th>Positive</th>
-            <th>Negative</th>
-            <th>Unconfirmed</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>{testingData.totaltested}</td>
-            <td style={{color: 'red'}}>{testingData.positive}</td>
-            <td style={{color: 'green'}}>{testingData.negative}</td>
-            <td style={{color: 'orange'}}>{testingData.unconfirmed}</td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
+    </>
   )
 }
