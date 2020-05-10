c89ee16b1d10d6773460ae363cd30b5312f8678a
Anand Raj <anandamritraj904@gmail.com>
2020-05-10 21:32:04 +0530

    Small refactoring and eslint fixes.

--- src/components/IndiaStateMap/IndiaMapControls.js
+++ src/components/IndiaStateMap/IndiaMapControls.js
@@ -3,18 +3,18 @@ import RangeSlider from './RangeSlider';
 import Switch from './Switch';
 
 const IndiaMapControls = ({
-  currentIndex, max, activeAttribute, handleSliderUpdate, mode, setMode
+  currentIndex, max, activeAttribute, handleSliderUpdate, mode, setMode,
 }) => (
   <div className="map-controls">
-  <Switch
-      mode={mode}
-      setMode={setMode}
-      activeAttribute={activeAttribute}
-    />
     <RangeSlider
       value={currentIndex}
       max={max}
       handleSliderUpdate={handleSliderUpdate}
+      activeAttribute={activeAttribute}
+    />
+    <Switch
+      mode={mode}
+      setMode={setMode}
       activeAttribute={activeAttribute}
     />
   </div>
--- src/components/IndiaStateMap/IndiaMapStats.js
+++ src/components/IndiaStateMap/IndiaMapStats.js
@@ -5,59 +5,77 @@ import { motion } from 'framer-motion';
 import * as population from '../../data/Population.json';
 
 function calculateCases(cases, mode, state) {
-  if (mode === 'normal')
-    return Number(cases);
+  if (mode === 'normal') { return Number(cases); }
   const casesPerMillion = Math.round((Number(cases) / population.default[state]) * 1000000);
   return casesPerMillion.toLocaleString('en');
 }
 
-const IndiaMapStats = ({ stats, handleAttributeClick, activeAttribute, mode, state }) => {
+const IndiaMapStats = ({
+  stats, handleAttributeClick, activeAttribute, mode, state,
+}) => {
   const { t } = useTranslation();
   const isNormal = mode === 'normal';
-  return <>
-    <div className="india-state-maps">
-      <motion.div
-        className="india-state-stats confirmed"
-        animate={{ scaleY: activeAttribute === 'confirmed' ? 1.1 : 1 }}
-        onClick={() => handleAttributeClick('confirmed')}
-      >
-        <h5>{calculateCases(stats.confirmed, mode, state)}</h5>
-        {!isNormal && <h6>{t('cases per million')}</h6>}
-        {isNormal && <h6>+{Number(stats.delta.confirmed).toLocaleString('en')}</h6>}
-        <h4>{t('Confirmed')}</h4>
-      </motion.div>
-      <motion.div
-        className="india-state-stats active"
-        animate={{ scaleY: activeAttribute === 'active' ? 1.1 : 1 }}
-        onClick={() => handleAttributeClick('active')}
-      >
-        <h5>{calculateCases(stats.active, mode, state)}</h5>
-        {!isNormal && <h6>{t('cases per million')}</h6>}
-        <h4>{t('Active')}</h4>
-      </motion.div>
-      <motion.div
-        animate={{ scaleY: activeAttribute === 'recovered' ? 1.1 : 1 }}
-        onClick={() => handleAttributeClick('recovered')}
-        className="india-state-stats recovered"
-      >
-        <h5>{calculateCases(stats.recovered, mode, state)}</h5>
-        {!isNormal && <h6>{t('cases per million')}</h6>}
-        {isNormal && <h6>+{Number(stats.delta.recovered).toLocaleString('en')}</h6>}
-        <h4>{t('Recovered')}</h4>
-      </motion.div>
-      <motion.div
-        className="india-state-stats deaths"
-        animate={{ scaleY: activeAttribute === 'deaths' ? 1.1 : 1 }}
-        onClick={() => handleAttributeClick('deaths')}
-      >
-        <h5>{calculateCases(stats.deaths, mode, state)}</h5>
-        {!isNormal && <h6>{t('cases per million')}</h6>}
-        {isNormal && <h6>+{Number(stats.delta.deaths).toLocaleString('en')}</h6>}
-        <h4>{t('Deaths')}</h4>
-      </motion.div>
-    </div>
-    <div className="instruction">{t('Click on the tiles to see cases in that category')}</div>
-  </>;
-}
+  return (
+    <>
+      <div className="india-state-maps">
+        <motion.div
+          className="india-state-stats confirmed"
+          animate={{ scaleY: activeAttribute === 'confirmed' ? 1.1 : 1 }}
+          onClick={() => handleAttributeClick('confirmed')}
+        >
+          <h5>{calculateCases(stats.confirmed, mode, state)}</h5>
+          {isNormal && (
+          <h6>
+            +
+            {Number(stats.delta.confirmed).toLocaleString('en')}
+          </h6>
+          )}
+          <h4>{t('Confirmed')}</h4>
+          {!isNormal && <h6>{t('per million')}</h6>}
+        </motion.div>
+        <motion.div
+          className="india-state-stats active"
+          animate={{ scaleY: activeAttribute === 'active' ? 1.1 : 1 }}
+          onClick={() => handleAttributeClick('active')}
+        >
+          <h5>{calculateCases(stats.active, mode, state)}</h5>
+          <h4>{t('Active')}</h4>
+          {!isNormal && <h6>{t('per million')}</h6>}
+        </motion.div>
+        <motion.div
+          animate={{ scaleY: activeAttribute === 'recovered' ? 1.1 : 1 }}
+          onClick={() => handleAttributeClick('recovered')}
+          className="india-state-stats recovered"
+        >
+          <h5>{calculateCases(stats.recovered, mode, state)}</h5>
+          {isNormal && (
+          <h6>
+            +
+            {Number(stats.delta.recovered).toLocaleString('en')}
+          </h6>
+          )}
+          <h4>{t('Recovered')}</h4>
+          {!isNormal && <h6>{t('per million')}</h6>}
+        </motion.div>
+        <motion.div
+          className="india-state-stats deaths"
+          animate={{ scaleY: activeAttribute === 'deaths' ? 1.1 : 1 }}
+          onClick={() => handleAttributeClick('deaths')}
+        >
+          <h5>{calculateCases(stats.deaths, mode, state)}</h5>
+          {isNormal && (
+          <h6>
+            +
+            {Number(stats.delta.deaths).toLocaleString('en')}
+          </h6>
+          )}
+          <h4>{t('Deaths')}</h4>
+          {!isNormal && <h6>{t('per million')}</h6>}
+        </motion.div>
+      </div>
+      <div className="instruction">{t('Click on the tiles to see cases in that category')}</div>
+    </>
+  );
+};
 
 export default React.memo(IndiaMapStats);
